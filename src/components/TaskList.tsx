9c26f939e7a2c845c337eb594e8f2ef51396c2d7
Filipe Gomes <filipe.gt1998@outlook.com>
2022-03-05 23:02:49 -0300

    Este commit add a versão final do código

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -24,10 +24,13 @@ export function TaskList() {
       }
   
       setTasks([...tasks, newTaskContent])
+      setNewTaskTitle('')
     }    
   }
 
   function handleToggleTaskCompletion(id: string) {    
+    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
     setTasks(tasks.map((task) => {
       if(task.id === id){
         return {...task, isComplete: !task.isComplete}
